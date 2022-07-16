609944c71fe6a24f300b3239fb4bc1205ef9dcb0
Nile Jocson <nikonova.nova@gmail.com>
2022-07-17 00:19:57 +0800

    feat: add Window class

--- include/Window.hpp
+++ include/Window.hpp
@@ -3,10 +3,55 @@
 
 
 
+
+
+
+#include <Windows.h>
+
+
+
+#include <string>
+
+
+
+
+
+
 class Window
 {
+public:
+	Window(int const width, int const height, std::string const &title) noexcept;
+	~Window() noexcept;
 
-}
+
+public:
+	auto restore()  -> void;
+	auto minimize() -> void;
+
+	auto poll_events() -> void;
+	auto wait_events() -> void;
+
+
+public:
+	auto get_hwnd() -> HWND;
+	auto get_hdc()  -> HDC;
+
+
+private:
+	static auto create_window_class()                                                      -> ATOM;
+	static auto create_window(int const width, int const height, std::string const &title) -> HWND;
+
+
+private:
+	static auto CALLBACK m_window_callback(HWND window, UINT message, WPARAM w_param, LPARAM l_param) -> LRESULT;
+	inline static ATOM m_window_class_id = 0;
+
+	HWND m_window;
+	HDC  m_dc;
+};
+
+
+
 
 
 
