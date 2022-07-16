c84ab538b6746a7a6772f51ef7b258367563e84c
Nile Jocson <nikonova.nova@gmail.com>
2022-07-16 22:26:06 +0800

    feat: add window_callback()

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,28 @@
+#include <Windows.h>
+
+
+
+
+
+
+auto CALLBACK window_callback(HWND window, UINT message, WPARAM w_param, LPARAM l_param) noexcept -> LRESULT;
+
+
+
+
+
+
 auto main() -> int
 {
 
+}
+
+
+
+
+
+
+auto CALLBACK window_callback(HWND window, UINT message, WPARAM w_param, LPARAM l_param) noexcept -> LRESULT
+{
+	return DefWindowProcA(window, message, w_param, l_param);
 }
