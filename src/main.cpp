70c32fb67b9283280190800bee7924b36376ca2b
Nile Jocson <nikonova.nova@gmail.com>
2022-07-17 17:12:23 +0800

    feat: test DrawWindow

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,8 @@
-#include <Windows.h>
+#include <cmath>
 
 
 
-#include <vector>
+#include <chrono>
 
 
 
@@ -13,88 +13,36 @@
 
 
 
-auto CALLBACK window_callback(HWND window, UINT message, WPARAM w_param, LPARAM l_param) noexcept -> LRESULT;
-
-
-
-
-
-
 auto main() -> int
 {
-	Window window(800, 800, "BitGL");
-
-
-
-	// SET UP RENDERING
-	auto                  window_dc       = window.get_hdc();
-	std::vector<COLORREF> framebuffer       (800 * 800, RGB(0, 0, 255)); // DIBs use BGR
-	auto                  framebuffer_dc  = CreateCompatibleDC(window_dc);
-	auto                  framebuffer_bmp = CreateBitmap(800,
-	                                                     800,
-	                                                     1,
-	                                                     sizeof(DWORD) * 8,
-	                                                     reinterpret_cast<void *>(framebuffer.data()));
-	SelectObject(framebuffer_dc, framebuffer_bmp);
-
-
-
+	DrawWindow window(800, 800, "BitGL");
 	window.restore();
 
-	MSG message;
+
+
+	float time_now = 0;
+	auto previous_time = std::chrono::high_resolution_clock::now();
+
+
+
 	while (window.is_open())
 	{
-		// RENDER TO window USING BitBlt
-		BitBlt(window_dc,
-		       0,
-		       0,
-		       800,
-		       800,
-		       framebuffer_dc,
-		       0,
-		       0,
-		       SRCCOPY);
+		auto current_time = std::chrono::high_resolution_clock::now();
+		time_now += std::chrono::duration_cast<std::chrono::milliseconds>(current_time - previous_time).count() / 1000.0f;
+		previous_time = current_time;
 
 
 
-		WaitMessage();
+		int r = (std::cos(time_now + 0      ) + 1) * 127;
+		int g = (std::cos(time_now + 2.0944 ) + 1) * 127;
+		int b = (std::cos(time_now + 4.18879) + 1) * 127;
 
-		while (PeekMessageA(&message, nullptr, 0, 0, PM_REMOVE))
-		{
-			TranslateMessage(&message);
-			DispatchMessageA(&message);
-		}
+		// Win32 uses BGR for this
+		window.clear_framebuffer(RGB(b, g, r));
+		window.draw();
+
+		window.poll_events();
 	}
-
-
-
-	DeleteObject(framebuffer_bmp);
-	DeleteDC(framebuffer_dc);
-
-
 
 	return EXIT_SUCCESS;
-}
-
-
-
-
-
-
-auto CALLBACK window_callback(HWND window, UINT message, WPARAM w_param, LPARAM l_param) noexcept -> LRESULT
-{
-	switch (message)
-	{
-	case WM_CLOSE:
-		{
-			auto window_is_valid = reinterpret_cast<bool *>(GetWindowLongPtrA(window, GWLP_USERDATA));
-			*window_is_valid = false;
-			return 0;
-		}
-
-	default:
-		{
-			return DefWindowProcA(window, message, w_param, l_param);
-		}
-	}
 }
