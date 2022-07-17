63895cb6c56abb3e379f63a363e3fb8ad0d86dfb
Nile Jocson <nikonova.nova@gmail.com>
2022-07-17 16:00:27 +0800

    feat: add DrawWindow class

--- include/DrawWindow.hpp
+++ include/DrawWindow.hpp
@@ -6,7 +6,49 @@
 
 
 
+#include <string>
+#include <vector>
 
+
+
+#include "Window.hpp"
+
+
+
+
+
+
+class DrawWindow : private Window
+{
+public:
+	DrawWindow(int const width, int const height, std::string const &title);
+	~DrawWindow();
+
+
+public:
+	auto clear_framebuffer(COLORREF const color)                      -> void;
+	auto render_point(int const x, int const y, COLORREF const color) -> void;
+
+	auto draw() -> void;
+
+
+public:
+	using Window::is_open;
+
+	using Window::restore;
+	using Window::minimize;
+
+	using Window::poll_events;
+	using Window::wait_events;
+
+
+private:
+	std::vector<COLORREF> framebuffer;
+	HDC                   framebuffer_dc;
+	HBITMAP               framebuffer_bitmap;
+
+	HBITMAP old_bitmap; // necessary to properly destruct framebuffer_dc
+};
 
 
 
