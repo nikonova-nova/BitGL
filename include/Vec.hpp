37c09e4de7f9393fc69119e6ae488286f0d21a07
Nile Jocson <nikonova.nova@gmail.com>
2022-07-17 21:49:29 +0800

    feat: add VecN aliases

--- include/Vec.hpp
+++ include/Vec.hpp
@@ -6,7 +6,25 @@
 
 
 
+#include <cstddef>
 
+
+
+#include <array>
+
+
+
+
+
+
+template<typename T>
+using Vec2 = std::array<T, 2>;
+
+template<typename T>
+using Vec3 = std::array<T, 3>;
+
+template<typename T>
+using Vec4 = std::array<T, 4>;
 
 
 
--- src/main.cpp
+++ src/main.cpp
@@ -6,6 +6,7 @@
 
 
 
+#include "Vec.hpp"
 #include "DrawWindow.hpp"
 
 
