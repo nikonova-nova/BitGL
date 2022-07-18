038a81fb443b3e26a99cc6fee97f68107acfd044
Nile Jocson <nikonova.nova@gmail.com>
2022-07-18 16:16:55 +0800

    feat: clear window to black for line visibility

--- src/main.cpp
+++ src/main.cpp
@@ -30,12 +30,12 @@ auto main() -> int
 		std::uint8_t g = (std::cos(time_now + 2.0944 ) + 1) * 127;
 		std::uint8_t b = (std::cos(time_now + 4.18879) + 1) * 127;
 
-		window.clear_colorbuffer({ r, g, b });
+		window.clear_colorbuffer({ 0, 0, 0 });
 
-		window.render_line({ 400, 799 }, { 0  , 400 }, { b, r, g });
-		window.render_line({ 0  , 400 }, { 400, 0   }, { b, r, g });
-		window.render_line({ 400, 0   }, { 799, 400 }, { b, r, g });
-		window.render_line({ 799, 400 }, { 400, 799 }, { b, r, g });
+		window.render_line({ 400, 799 }, { 0  , 400 }, { r, g, b });
+		window.render_line({ 0  , 400 }, { 400, 0   }, { r, g, b });
+		window.render_line({ 400, 0   }, { 799, 400 }, { r, g, b });
+		window.render_line({ 799, 400 }, { 400, 799 }, { r, g, b });
 
 		window.draw();
 
