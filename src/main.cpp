23a693dae7ed2be473e21b4abcc943722a10fdac
Nile Jocson <nikonova.nova@gmail.com>
2022-07-18 21:43:48 +0800

    feat: stress test rendering

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,5 @@
 #include <cmath>
+#include <cstdio>
 
 
 
@@ -21,10 +22,15 @@ auto main() -> int
 	window.restore();
 
 
+	auto time_before = BitGL::Window::get_time();
 
 	while (window.is_open())
 	{
 		auto time_now = BitGL::Window::get_time();
+		auto delta_time = time_now - time_before;
+		time_before = time_now;
+
+		printf("T: %f\tFPS: %f\n", delta_time, 1 / delta_time);
 
 		std::uint8_t r = (std::cos(time_now + 0      ) + 1) * 127;
 		std::uint8_t g = (std::cos(time_now + 2.0944 ) + 1) * 127;
@@ -32,10 +38,11 @@ auto main() -> int
 
 		window.clear_colorbuffer({ 0, 0, 0 });
 
-		window.render_line({ 400, 799 }, { 0  , 400 }, { r, g, b });
-		window.render_line({ 0  , 400 }, { 400, 0   }, { r, g, b });
-		window.render_line({ 400, 0   }, { 799, 400 }, { r, g, b });
-		window.render_line({ 799, 400 }, { 400, 799 }, { r, g, b });
+		for (int i = 0; i < 800; ++i)
+		{
+			window.render_line({ i, 0 }, { i, 799 }, { r, g, b });
+			window.render_line({ 0, i }, { 799, i }, { g, b, r });
+		}
 
 		window.draw();
 
