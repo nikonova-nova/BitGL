ecd673acf25102753ef806e609c017fe73a5c93c
Nile Jocson <nikonova.nova@gmail.com>
2022-07-19 11:20:44 +0800

    feat: add Triangle2D alias

---
+++ include/Triangle.hpp
@@ -0,0 +1,26 @@
+#ifndef NIKONOVA_NOVA_BITGL_INCLUDE_POINT_HPP
+#define NIKONOVA_NOVA_BITGL_INCLUDE_POINT_HPP
+
+
+
+
+
+
+#include "Point.hpp"
+
+
+
+
+
+
+namespace BitGL
+{
+	using Triangle2D = Vec3<Point2D>;
+}
+
+
+
+
+
+
+#endif
