65caaf98d7c80c85c8f6a51a715e4c302832596f
Nile Jocson <nikonova.nova@gmail.com>
2022-07-19 12:12:04 +0800

    feat: test draw_triangle

--- src/main.cpp
+++ src/main.cpp
@@ -32,17 +32,43 @@ auto main() -> int
 
 		printf("T: %f\tFPS: %f\n", delta_time, 1 / delta_time);
 
-		std::uint8_t r = (std::cos(time_now + 0      ) + 1) * 127;
-		std::uint8_t g = (std::cos(time_now + 2.0944 ) + 1) * 127;
-		std::uint8_t b = (std::cos(time_now + 4.18879) + 1) * 127;
-
 		window.clear_colorbuffer({ 0, 0, 0 });
 
-		for (int i = 0; i < 800; ++i)
-		{
-			window.render_line({ i, 0 }, { i, 799 }, { r, g, b });
-			window.render_line({ 0, i }, { 799, i }, { g, b, r });
-		}
+		BitGL::Point2D front_top_left      = { 99 , 299 };
+		BitGL::Point2D front_top_right     = { 499, 299 };
+		BitGL::Point2D front_bottom_right  = { 499, 699 };
+		BitGL::Point2D front_bottom_left   = { 99 , 699 };
+
+		BitGL::Point2D back_top_left      = { 299, 99  };
+		BitGL::Point2D back_top_right     = { 699, 99  };
+		BitGL::Point2D back_bottom_right  = { 699, 499 };
+		BitGL::Point2D back_bottom_left   = { 299, 499 };
+
+		BitGL::Triangle2D front_a = { front_top_left, front_top_right, front_bottom_right };
+		BitGL::Triangle2D front_b = { front_bottom_right, front_bottom_left, front_top_left };
+
+		BitGL::Triangle2D back_a = { back_top_left, back_top_right, back_bottom_right };
+		BitGL::Triangle2D back_b = { back_bottom_right, back_bottom_left, back_top_left };
+
+		BitGL::Triangle2D top_a = { back_top_left, back_top_right, front_top_right };
+		BitGL::Triangle2D top_b = { front_top_right, front_top_left, back_top_left };
+
+		BitGL::Triangle2D bottom_a = { back_bottom_left, back_bottom_right, front_bottom_right };
+		BitGL::Triangle2D bottom_b = { front_bottom_right, front_bottom_left, back_bottom_left };
+
+		BitGL::ColorRGB cyan = { 0, 255, 255 };
+
+		window.render_triangle(front_a, cyan);
+		window.render_triangle(front_b, cyan);
+
+		window.render_triangle(back_a, cyan);
+		window.render_triangle(back_b, cyan);
+
+		window.render_triangle(top_a, cyan);
+		window.render_triangle(top_b, cyan);
+
+		window.render_triangle(bottom_a, cyan);
+		window.render_triangle(bottom_b, cyan);
 
 		window.draw();
 
