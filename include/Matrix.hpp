be5756bdbea94b964a2b6abe7894ab859ecb8869
Nile Jocson <nikonova.nova@gmail.com>
2022-07-22 15:30:54 +0800

    feat: add more Matrix aliases

--- include/Matrix.hpp
+++ include/Matrix.hpp
@@ -54,11 +54,28 @@ namespace BitGL
 
 
 	template<typename T>
-	using Matrix2 = Matrix<T, 2>;
+	using Matrix2   = Matrix<T, 2>;
+	using BMatrix2  = Matrix2<bool>;
+	using IMatrix2  = Matrix2<int>;
+	using FMatrix2  = Matrix2<float>;
+	using DMatrix2  = Matrix2<double>;
+	using LDMatrix2 = Matrix2<long double>;
+
 	template<typename T>
-	using Matrix3 = Matrix<T, 3>;
+	using Matrix3   = Matrix<T, 3>;
+	using BMatrix3  = Matrix3<bool>;
+	using IMatrix3  = Matrix3<int>;
+	using FMatrix3  = Matrix3<float>;
+	using DMatrix3  = Matrix3<double>;
+	using LDMatrix3 = Matrix3<long double>;
+
 	template<typename T>
-	using Matrix4 = Matrix<T, 4>;
+	using Matrix4   = Matrix<T, 4>;
+	using BMatrix4  = Matrix4<bool>;
+	using IMatrix4  = Matrix4<int>;
+	using FMatrix4  = Matrix4<float>;
+	using DMatrix4  = Matrix4<double>;
+	using LDMatrix4 = Matrix4<long double>;
 }
 
 
